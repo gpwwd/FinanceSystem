3120f1c9c243c6c7d53b96837e6a6f21ff870aa0
maks <petuhovmaksim073@gmail.com>
2025-03-08 16:48:06 +0300

    feat(database): create transaction table
    Temporary solution: from_type and from_id is null, as the business logic is still being defined.

---
+++ src/main/resources/db/migration/V10__transaction.sql
@@ -0,0 +1,12 @@
+CREATE TABLE transaction (
+     id SERIAL PRIMARY KEY,
+     from_entity_id BIGINT,
+     from_type VARCHAR(50)
+         CHECK (from_type IS NULL OR from_type IN ('ACCOUNT', 'DEPOSIT', 'LOAN')),
+--  `                       null is temporary(because of the unfinished loan issue logic)
+     to_entity_id BIGINT NOT NULL,
+     to_type VARCHAR(50) NOT NULL CHECK (to_type IN ('ACCOUNT', 'DEPOSIT', 'LOAN')),
+     amount DECIMAL(19,2) NOT NULL CHECK (amount > 0),
+     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     status VARCHAR(20) NOT NULL CHECK (status IN ('PENDING', 'COMPLETED', 'FAILED'))
+);
